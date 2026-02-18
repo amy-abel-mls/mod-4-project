912dda802022ad230897edb6c3d858f81f72953c
amy14rubio <amy14rubio@gmail.com>
2026-02-17 20:03:08 -0500

    modified getGenres API and completed book search features

--- src/fetch-helpers.js
+++ src/fetch-helpers.js
@@ -1,6 +1,6 @@
 export const getGenres = async (genre) => {
   try {
-    const response = await fetch(`https://openlibrary.org/search.json?q=${genre}`);
+    const response = await fetch(`https://openlibrary.org/subjects/${genre}.json`);
     if (!response.ok) throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
     const data = await response.json();
     return { data, error: null };
@@ -10,14 +10,38 @@ export const getGenres = async (genre) => {
   }
 };
 
-export const getSingleBook = async (id) => {
+export const getSingleBook = async (key) => {
   try {
-    const response = await fetch(`https://openlibrary.org${id}.json`);
+    const response = await fetch(`https://openlibrary.org${key}.json`);
     if (!response.ok) throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
     const data = await response.json();
     return { data, error: null };
   } catch (error) {
     console.log(error.message);
+    return { data: null, error };
+  }
+};
+
+export const getBookCover = async (olid) => {
+  try {
+    const response = await fetch(`https://covers.openlibrary.org/b/olid/${olid}-L.jpg`);
+    if (!response.ok) throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
+    const data = await response.json();
+    return { data, error: null };
+  } catch (error) {
+    console.error(error.message);
+    return { data: null, error };
+  }
+};
+
+export const searchBooks = async (query) => {
+  try {
+    const response = await fetch(`https://openlibrary.org/search.json?q=${query}&sort=rating`);
+    if (!response.ok) throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
+    const data = await response.json();
+    return { data, error: null };
+  } catch (error) {
+    console.error(error.message);
     return { data: null, error };
   }
 };
