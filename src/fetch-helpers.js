659b79155ad730231f084e81cca213357eaed365
amy14rubio <amy14rubio@gmail.com>
2026-02-17 21:50:51 -0500

    modified each function to return cover data

--- src/fetch-helpers.js
+++ src/fetch-helpers.js
@@ -3,7 +3,10 @@ export const getGenres = async (genre) => {
     const response = await fetch(`https://openlibrary.org/subjects/${genre}.json`);
     if (!response.ok) throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
     const data = await response.json();
-    return { data, error: null };
+    let cover = [];
+    for (const work of data.works)
+      cover.push(`https://covers.openlibrary.org/b/olid/${work.cover_edition_key}-L.jpg`);
+    return { data: [data, cover], error: null };
   } catch (error) {
     console.log(error.message);
     return { data: null, error };
@@ -15,21 +18,10 @@ export const getSingleBook = async (key) => {
     const response = await fetch(`https://openlibrary.org${key}.json`);
     if (!response.ok) throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
     const data = await response.json();
-    return { data, error: null };
+    const cover = `https://covers.openlibrary.org/b/id/${data.covers[0]}-L.jpg`;
+    return { data: [data, cover], error: null };
   } catch (error) {
     console.log(error.message);
-    return { data: null, error };
-  }
-};
-
-export const getBookCover = async (olid) => {
-  try {
-    const response = await fetch(`https://covers.openlibrary.org/b/olid/${olid}-L.jpg`);
-    if (!response.ok) throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
-    const data = await response.json();
-    return { data, error: null };
-  } catch (error) {
-    console.error(error.message);
     return { data: null, error };
   }
 };
@@ -39,7 +31,10 @@ export const searchBooks = async (query) => {
     const response = await fetch(`https://openlibrary.org/search.json?q=${query}&sort=rating`);
     if (!response.ok) throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
     const data = await response.json();
-    return { data, error: null };
+    let cover = [];
+    for (const doc of data.docs)
+      cover.push(`https://covers.openlibrary.org/b/olid/${doc.cover_edition_key}-L.jpg`);
+    return { data: [data, cover], error: null };
   } catch (error) {
     console.error(error.message);
     return { data: null, error };
